a51aedbf1172c0bdb74ec5cc36c90fefedf915c9
JHorrigan <jahorrigan1411@gmail.com>
2020-09-27 13:12:29 +0100

    Use Exact Routes for Page Components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
 
-
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 
+import { Route, Switch } from 'react-router-dom';
+
 function App() {
   return ( 
     <>
-      <Home></Home>
-      <Rooms></Rooms>
-      <SingleRoom></SingleRoom>
-      <Error></Error>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/rooms/' component={Rooms}/>
+      <Route exact path='/single-room/' component={SingleRoom}/>
     </>
   );
 }
