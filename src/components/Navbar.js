33df35ab7bbaade15b561ea834795b9bc53207d5
JHorrigan <jahorrigan1411@gmail.com>
2020-09-27 19:40:29 +0100

    Add Nav Links & Button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,23 @@ export default class Navbar extends Component {
                         <Link to="/">
                             <img src={logo} alt="Beach Resort" />
                         </Link>
-                    </div>            
+                        <button 
+                            type="button"
+                            className="nav-btn"
+                            onClick={this.handleToggle}
+                        >
+                            <FaAlignRight className="nav-icon" />
+                        </button>
+                    </div>
+                    <ul className={this.state.isOpen?"nav-links show-nav"
+                    :"nav-links"}>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/rooms">Rooms</Link>                            
+                        </li> 
+                    </ul>        
                 </div>
             </nav>
         )
