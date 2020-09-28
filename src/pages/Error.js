e3c0eb5d1972b1810316abfccb8680c79407e793
JHorrigan <jahorrigan1411@gmail.com>
2020-09-28 09:22:39 +0100

    Show Hero in Error Page with Default Hero Prop

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,6 @@
 import React from 'react'
+import Hero from '../components/Hero';
 
 export default function Error() {
-    return (
-        <div>
-            Message from Error Page
-        </div>
-    )
+    return <Hero />
 }
