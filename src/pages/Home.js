1a3b9838efc080b104ab9a056190abe7697911cc
JHorrigan <jahorrigan1411@gmail.com>
2020-09-28 08:07:46 +0100

    Show Hero in Home Page with Default Hero Prop

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import Hero from '../components/Hero';
 
 export default function Home() {
     return (
-        <div>
-            Message from Home Page
-        </div>
+        <Hero />
     )
 }
 
