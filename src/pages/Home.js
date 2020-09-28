cd7bfc3a4c49b5bc06b8e9a77c778932fd8a680b
JHorrigan <jahorrigan1411@gmail.com>
2020-09-28 18:11:21 +0100

    Show & Pass Props to Banner on Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
-        <Hero />
+        <Hero>
+           <Banner title="Luxurious Rooms" subtitle="Deluxe Rooms starting at £50">
+                <Link to='/rooms' className="btn-primary">
+                    Our Rooms    
+                </Link>   
+            </Banner> 
+        </Hero>
     )
 }
 
