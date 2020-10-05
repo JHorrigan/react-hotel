cd15d16905b88c49dba4471d366922a396dc0482
JHorrigan <jahorrigan1411@gmail.com>
2020-10-05 16:22:59 +0100

    Use Context in FeaturedRooms Component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import { RoomContext } from '../Context'
 
 export default class FeaturedRooms extends Component {
+    static contextType = RoomContext;
     render() {
+        const { name, age } = this.context;
         return (
             <div>
-                Message from Featured Rooms
+                Message from Featured Rooms ({name}, {age})
             </div>
         )
     }
