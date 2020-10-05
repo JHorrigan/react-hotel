2aa3723960e06238d73dd0900b21c3ffbdc57500
JHorrigan <jahorrigan1411@gmail.com>
2020-10-05 15:35:58 +0100

    Create Featured Rooms Class Component

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class FeaturedRooms extends Component {
+    render() {
+        return (
+            <div>
+                Message from Featured Rooms
+            </div>
+        )
+    }
+}
