74a860fe2967a0a73a83d89ab6b3bbb468e5a664
JHorrigan <jahorrigan1411@gmail.com>
2020-10-05 18:46:48 +0100

    Format & Flatten Room Data

--- src/Context.js
+++ src/Context.js
@@ -15,6 +15,17 @@ class RoomProvider extends Component {
         let rooms = this.formatData(items)
     }
 
+    formatData(items) {
+        let tempItems = items.map(item => {
+            let id = item.sys.id
+            let images = item.fields.images.map(image =>
+                image.fields.file.url);
+            let room = {...item.fields, images, id}
+            return room
+        });
+        return tempItems
+    }
+
     render() {
         return (
             <RoomContext.Provider value={{...this.state}}>
--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react'
 import { RoomContext } from '../Context'
 
 export default class FeaturedRooms extends Component {
-    static contextType = RoomContext;
     render() {
-        const { name, age } = this.context;
         return (
             <div>
-                Message from Featured Rooms ({name}, {age})
+                Message from Featured Rooms
             </div>
         )
     }
