49143455f38a2aa369ba01ea076fdcaac65c3616
JHorrigan <jahorrigan1411@gmail.com>
2020-10-05 21:03:56 +0100

    Add Link Component to Room

--- src/components/Room.js
+++ src/components/Room.js
@@ -12,6 +12,10 @@ export default function Room({room}) {
                     <h6>£{price}</h6>
                     <p>per night</p>
                 </div>
+                <Link to={`/rooms/${slug}`}
+                className="btn-primary room-link">
+                    Features
+                </Link>
             </div>
         </article>
     )
