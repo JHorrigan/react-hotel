9a4c5973bfe9fc1c32f74fa31f927c053c8b1242
JHorrigan <jahorrigan1411@gmail.com>
2020-10-06 08:21:53 +0100

    Imports & Constructor for Single Room

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react'
+import defaultBcg from '../images/room-1.jpeg'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
+import  { RoomContext } from '../Context'
 
 export default class SingleRoom extends Component {
+
+    constructor(props) {
+        super(props)
+        console.log(this.props);
+    }
+
+    // Without context we would use slug in props
+    // from router to make API call
+    //componentDidMount() {
+        // API call using slug
+    //}
+
     render() {
         return (
             <div>
