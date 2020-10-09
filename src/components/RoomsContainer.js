bfcc6faea49e5c0c85b3ca8f714aae3fb1106e5b
JHorrigan <jahorrigan1411@gmail.com>
2020-10-09 09:03:56 +0100

    Import New Rooms Components Into Rooms Container

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import RoomsFilter from './RoomsFilter'
+import RoomsList from './RoomsList'
 
 export default function RoomsContainer() {
     return (
         <div>
             Message from Rooms Container
+            <RoomsFilter />
+            <RoomsList />
         </div>
     )
 }
