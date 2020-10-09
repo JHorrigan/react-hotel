80129ab3db45b5a2bb6c50c24150a775cbccfd40
JHorrigan <jahorrigan1411@gmail.com>
2020-10-09 07:59:09 +0100

    Add Extras List Map to Single Room Page Article

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -90,6 +90,14 @@ export default class SingleRoom extends Component {
                     </article>
                 </div>
             </section>
+            <section className="room-extras">
+                <h6>Extras</h6>
+                <ul className="extras">
+                    {extras.map((item, index) => {
+                        return <li key={index}>- {item}</li>;
+                    })}
+                </ul>
+            </section>
             </>
         )
     }
