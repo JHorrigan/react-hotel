fb1b8039881353f6846ce9595d8914679a0e7544
JHorrigan <jahorrigan1411@gmail.com>
2020-10-10 16:49:33 +0100

    Populate Room Type Drop Down with Unique Values

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -3,16 +3,38 @@ import { useContext } from 'react'
 import { RoomContext } from '../Context'
 import Title from '../components/Title'
 
-export default function RoomsFilter() {
+const getUnique = (items, value) => {
+    return [...new Set(items.map(item => item[value]))]
+}
+
+export default function RoomsFilter({rooms}) {
     const context = useContext(RoomContext);
     const {
         handleChange, type, capacity, price,
         minPrice, maxPrice, minSize, maxSize,
         breakfast, pets
     } = context
+    // get unique types
+    let types = getUnique(rooms, 'type')
+    // add all option to types
+    types = ['all',...types]
+    // map to jsx
+    types = types.map((item, index) => {
+        return <option value={item} key={index}>{item}</option>
+    })
     return (
-        <div>
-            Message from Rooms Filter
-        </div>
+        <section className="filter-container">
+            <Title title="Search Rooms" />
+            <form className="filter-form">
+                <div className="form-group">
+                    <label htmlFor="type">Room Type</label>
+                    <select name="type" id="type"
+                    value={type} className="form-control"
+                    onChange={handleChange}>
+                        {types}
+                    </select>
+                </div>
+            </form>
+        </section>
     )
 }
