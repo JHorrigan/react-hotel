ce8d04b08d7d09c05b682641542a2cb714daf3e1
JHorrigan <jahorrigan1411@gmail.com>
2020-10-10 16:21:20 +0100

    Use Context & Deconstruct

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useContext } from 'react'
+import { RoomContext } from '../Context'
+import Title from '../components/Title'
 
 export default function RoomsFilter() {
+    const context = useContext(RoomContext);
+    const {
+        handleChange, type, capacity, price,
+        minPrice, maxPrice, minSize, maxSize,
+        breakfast, pets
+    } = context
     return (
         <div>
             Message from Rooms Filter
