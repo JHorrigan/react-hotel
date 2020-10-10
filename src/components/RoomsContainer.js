4254ad454695765c863467c63fec265ca991adab
JHorrigan <jahorrigan1411@gmail.com>
2020-10-10 12:29:24 +0100

    Create Higher Order Component withRoomConsumer & Wrapper

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,27 +1,21 @@
 import React from 'react'
 import RoomsFilter from './RoomsFilter'
 import RoomsList from './RoomsList'
-import { RoomConsumer } from '../Context'
+import { withRoomConsumer } from '../Context'
 import Loading from  './Loading'
 
-export default function RoomsContainer() {
+function RoomsContainer({context}) {
+    const {loading, sortedRooms, rooms} = context;
+    if(loading) {
+        return <Loading />;
+    }
     return (
-        <RoomConsumer>
-            {
-                (value) => {
-                    const {loading, sortedRooms, rooms} = value;
-                    if(loading) {
-                        return <Loading />;
-                    }
-                    return (
-                        <div>
-                            Message from Rooms Container
-                            <RoomsFilter rooms={rooms} />
-                            <RoomsList rooms={sortedRooms} />
-                        </div>
-                    )
-                }
-            }
-        </RoomConsumer>
+        <div>
+            Message from Rooms Container
+            <RoomsFilter rooms={rooms} />
+            <RoomsList rooms={sortedRooms} />
+        </div>
     )
 }
+
+export default withRoomConsumer(RoomsContainer)
