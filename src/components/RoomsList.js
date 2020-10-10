eb40cd01023949dccce1557a166a02de3b0e262c
JHorrigan <jahorrigan1411@gmail.com>
2020-10-10 13:03:49 +0100

    Pass Rooms to Rooms List & Check for Zero Rooms

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import Room from './Room'
 
-export default function RoomsList() {
+export default function RoomsList({rooms}) {
+    if(rooms.length === 0){
+        return (
+            <div className="empty-search">
+                <h3>Unfortunately no rooms matched your 
+                search parameters</h3>
+            </div>
+        )
+    }
     return (
         <div>
             Message from Rooms List
