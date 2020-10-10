fdfbac1a7f857e4db7ffa6b8bdc300c23eceb861
JHorrigan <jahorrigan1411@gmail.com>
2020-10-10 13:50:21 +0100

    Setup General Handle Change Function in Context

--- src/Context.js
+++ src/Context.js
@@ -56,12 +56,24 @@ class RoomProvider extends Component {
         return room;
     }
 
+    handleChange = event => {
+        const type = event.target.type
+        const name = event.target.name
+        const value = event.target.value
+        console.log(type, name, value);
+    }
+
+    filterRooms = () => {
+        console.log('Message from filterRooms');
+    };
+
     render() {
         return (
             <RoomContext.Provider
                 value={{
                         ...this.state,
-                        getRoom: this.getRoom
+                        getRoom: this.getRoom,
+                        handleChange: this.handleChange
                     }}
             >
                 {this.props.children}
